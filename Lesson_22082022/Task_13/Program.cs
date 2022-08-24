6165020a3f19a362cb3b55db5cc034e4c16a259c
ubivakus <stepanov.alex.2012@gmail.com>
2022-08-24 21:18:51 +0300

    Added Task_13

--- Lesson_22082022/Task_13/Program.cs
+++ Lesson_22082022/Task_13/Program.cs
@@ -7,3 +7,25 @@
 
 32679 -> 6
 */
+
+Console.Clear();
+int number; 
+int orderOfDigit = 2;
+
+System.Console.WriteLine("Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+System.Console.Write("Введите любое целое число: ");
+number = Convert.ToInt32(Console.ReadLine());
+string stringOfNumber = number.ToString();
+System.Console.WriteLine($"Ваше число: {stringOfNumber}");
+
+if (stringOfNumber.Length > orderOfDigit)
+{
+    System.Console.WriteLine($"Третья цифра вашего числа - {stringOfNumber[orderOfDigit]}");
+} else
+{
+    System.Console.WriteLine("В вашем числе нет третьей цифры! Попробуйте другое число");
+}
+System.Console.WriteLine("Программа завершена.");
+
+
+
