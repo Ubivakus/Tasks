b414f57326948df0b15b6655718b4ab4937936f8
ubivakus <stepanov.alex.2012@gmail.com>
2022-09-01 19:46:56 +0300

    Check 010922

---
+++ Lesson_29082022/Task_27/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+Console.Clear();
+
+
+System.Console.Write("Введите любое натуральное число: ");
+string a = "59";
+int sum = 0;
+System.Console.WriteLine(a);
+System.Console.WriteLine(Convert.ToInt32(a[0]));
+System.Console.WriteLine(a[1]);
+
+
+
