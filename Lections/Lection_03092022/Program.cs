9816583cf8aa272b3443c692b6d7669545665d7d
ubivakus <stepanov.alex.2012@gmail.com>
2022-09-04 22:16:24 +0300

    Added Folder Lections

---
+++ Lections/Lection_03092022/Program.cs
@@ -0,0 +1,31 @@
+﻿//Двумерные массивы и рекурсия
+
+Console.Clear();
+
+/*
+string[,] table = new string[2, 5]; //Массив строк
+table[1,2] = "слово";
+
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        System.Console.WriteLine($"-{table[rows, columns]}-");
+    }
+}
+*/
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int[3, 4]; //Матрица из чисел
+PrintArray(matrix);
