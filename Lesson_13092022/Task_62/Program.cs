23eaff67e769fd9772794380de4df435a6d6858a
ubivakus <stepanov.alex.2012@gmail.com>
2022-09-17 23:09:36 +0300

    Added Task_62

--- Lesson_13092022/Task_62/Program.cs
+++ Lesson_13092022/Task_62/Program.cs
@@ -4,3 +4,49 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+Console.Clear();
+
+System.Console.WriteLine("Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.\n\n"
+                        + "Например, на выходе получается вот такой массив:\n");
+
+int[,] massive = FillSpiralMassive(4, 4);
+PrintArray(massive);
+
+
+// Функции будут здесь.
+
+int[,] FillSpiralMassive(int row, int col)
+{
+    int[,] array = new int[row, col];
+    int number = row * col;
+    int element = 1;
+    int i = 0, j = 0;
+    while (element <= number)
+    {
+        array[i, j] = element++;
+        if (i <= j + 1 && i + j < col - 1)
+            j++;
+        else if (i < j && i + j >= row - 1)
+            i++;
+        else if (i >= j && i + j > col - 1)
+            j--;
+        else
+            i--;
+    }
+    return array;
+}
+
+
+void PrintArray(int[,] array) // Печать массива.
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}     ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
