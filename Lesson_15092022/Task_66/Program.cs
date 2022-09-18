6e1961a9e9bdf695fac6c066c02b183244377a81
ubivakus <stepanov.alex.2012@gmail.com>
2022-09-18 15:14:03 +0300

    Added Task_66

--- Lesson_15092022/Task_66/Program.cs
+++ Lesson_15092022/Task_66/Program.cs
@@ -2,3 +2,18 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.Clear();
+System.Console.WriteLine("Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.\n");
+
+int sum = 0;
+System.Console.WriteLine(SumOfNumbers(4, 8));
+
+
+// Функция будет здесь.
+
+int SumOfNumbers(int firstNumber, int secondNumber)
+{
+    if (firstNumber > secondNumber) return sum;
+    else return sum += firstNumber + SumOfNumbers(firstNumber + 1, secondNumber);
+}
