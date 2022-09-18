74a2125895427a369f9d314d547f37b840d38250
ubivakus <stepanov.alex.2012@gmail.com>
2022-09-18 14:24:05 +0300

    Added Task_64

---
+++ Lesson_15092022/Task_64/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+Console.Clear();
+
+System.Console.WriteLine("Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.\n");
+int sequence = SequenceOfNumbers(1, 10);
+
+
+// Функция будет здесь.
+
+int SequenceOfNumbers(int firstNumber, int secondNumber)
+{
+    Console.Write(firstNumber + ", ");
+    if (firstNumber == secondNumber)
+    {
+        return firstNumber;
+    }
+    else
+    {
+        return firstNumber + SequenceOfNumbers(firstNumber + 1, secondNumber);
+    }
+}
+
+
